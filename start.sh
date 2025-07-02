5b78aa43afc4aa17aee2ea9d03c2957d8d1f6e4a
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-07-02 09:20:50 +0100

    Remove superuser creation logic from start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
-
 chmod +x ./pocketbase
-
-ADMIN_EMAIL=${ADMIN_EMAIL:-admin@example.com}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-supersecure}
-
-# Create superuser if not already created
-if ! ./pocketbase superuser list | grep -q "$ADMIN_EMAIL"; then
-  echo "🔐 Creating default superuser account..."
-  ./pocketbase superuser upsert "$ADMIN_EMAIL" "$ADMIN_PASSWORD"
-else
-  echo "✅ Superuser already exists."
-fi
-
-# Start the PocketBase server
 ./pocketbase serve --http 0.0.0.0:$PORT
