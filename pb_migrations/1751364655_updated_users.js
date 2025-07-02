c512cdc3b4831845e42795ae64ba8e56ea48081d
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-07-02 08:28:42 +0100

    initial commit

---
+++ pb_migrations/1751364655_updated_users.js
@@ -0,0 +1,64 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "id": "",
+        "name": "",
+        "username": ""
+      }
+    }
+  }, collection)
+
+  // update field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1579384326",
+    "max": 255,
+    "min": 0,
+    "name": "firstName",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "id": "name",
+        "name": "name",
+        "username": "name"
+      }
+    }
+  }, collection)
+
+  // update field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1579384326",
+    "max": 255,
+    "min": 0,
+    "name": "name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
